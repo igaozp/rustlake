d77eb957f159eb0103364c412dabd2b5bb8f557c
igaozp <igaozp@hotmail.com>
2022-01-04 22:10:41 +0800

    Functions

---
+++ function/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    println!("Hello, world!");
+    another_function(32);
+    print_labeled_measurement(5, 'h');
+
+    let x = 5;
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {}", y);
+
+    let x = five();
+    let y = plus_one(x);
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {}", x);
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {}: {}", unit_label, value);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
