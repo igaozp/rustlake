2b35af575f26637e917ebf9c34d80001be1a1034
igaozp <igaozp@hotmail.com>
2022-01-15 17:51:09 +0800

    Defining an Enum

---
+++ defining-an-enum/src/main.rs
@@ -0,0 +1,57 @@
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    route(four);
+    route(six);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    route(home.kind);
+    route(loopback.kind);
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None;
+}
+
+fn route(ip_kind: IpAddrKind) {
+    match ip_kind {
+        IpAddrKind::V4 => println!("IPv4"),
+        IpAddrKind::V6 => println!("IPv6"),
+    }
+}
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", self);
+    }
+}
