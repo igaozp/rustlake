9696760bb8bdc39aaa2782ea199b92801f49f8e1
igaozp <igaozp@hotmail.com>
2022-01-22 22:27:06 +0800

    Vectors

---
+++ vectors/src/main.rs
@@ -0,0 +1,48 @@
+fn main() {
+    let v: Vec<i32> = Vec::new();
+    let v = vec![1, 2, 3];
+
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(4);
+    v.push(3);
+    v.push(2);
+    v.push(1);
+
+    let v = vec![1, 2, 3, 4, 5];
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
+
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let mut v = vec![100, 32, 37];
+    for i in &mut v {
+        *i += 50;
+        println!("{}", i);
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+    for i in &row {
+        println!("{:?}", i);
+    }
+}
+
+#[derive(Debug)]
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
