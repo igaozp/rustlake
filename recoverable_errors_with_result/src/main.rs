2a1e0c2fcd6afb93ef7e9c27d0ca0d73ecad17f6
Pzoagi <igaozp@hotmail.com>
2022-02-20 02:55:05 +0000

    Recoverable errors example with boardcast

--- recoverable_errors_with_result/src/main.rs
+++ recoverable_errors_with_result/src/main.rs
@@ -1,5 +1,8 @@
 use std::fs::File;
 use std::io::ErrorKind;
+use std::io::Error;
+use std::io::{self, Read};
+use std::fs;
 
 fn main() {
     let f = File::open("hello.txt");
@@ -33,4 +36,29 @@ fn main() {
 
     // return file or auto panic with custom message
     let f = File::open("hello.txt").expect("Failed to open hello.txt");
+}
+
+// boardcast errors
+fn read_username_from_file() -> Result<String, Error> {
+    // let f = File::open("hello.txt");
+    // let mut f = match f {
+    //     Ok(file) => file,
+    //     Err(e) => return Err(e)
+    // };
+    // let mut s = String::new();
+    // match f.read_to_string(&mut s) {
+    //     Ok(_) => Ok(s),
+    //     Err(e) => Err(e),
+    // }
+
+    // let mut f = File::open("hello.txt")?;
+    // let mut s = String::new();
+    // f.read_to_string(&mut s)?;
+    // Ok(s)
+
+    // let mut s = String::new();
+    // File::open("hello.txt")?.read_to_string(&mut s)?;
+    // Ok(s)
+
+    fs::read_to_string("hello.txt")
 }
