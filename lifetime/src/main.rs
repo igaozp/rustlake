afdfb76ccd6dbb6883c5074f045a2e2b0014bc7d
Pzoagi <igaozp@hotmail.com>
2022-02-20 07:28:57 +0000

    Lifetime example

---
+++ lifetime/src/main.rs
@@ -0,0 +1,47 @@
+use std::fmt::Display;
+
+fn main() {
+    let s1 = String::from("abcd");
+    let s2 = "xyz";
+    let result = longest(s1.as_str(), s2);
+    println!("The longest string is {}", result);
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    println!("The level is {}", i.level());
+
+    let s: &'static str = "I have a static lifetime.";
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
