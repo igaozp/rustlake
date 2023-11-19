e6c261a0e190831260bab77307ea2f86b7ff259d
igaozp <igaozp@hotmail.com>
2023-11-19 10:46:22 +0800

    feat: reduce frequency of time profiling

--- wasm/wasm-game-of-life/www/index.js
+++ wasm/wasm-game-of-life/www/index.js
@@ -22,7 +22,9 @@ const renderLoop = () => {
     fps.render();
 
     // debugger;
-    universe.tick();
+    for (let i = 0; i < 9; i++) {
+        universe.tick();
+    }
 
     drawGrid();
     drawCells();
@@ -82,11 +84,30 @@ const drawCells = () => {
 
     ctx.beginPath();
 
+    ctx.fillStyle = ALIVE_COLOR;
     for (let row = 0; row < height; row++) {
         for (let col = 0; col < width; col++) {
             const idx = getIndex(row, col);
+            if (cells[idx] !== Cell.Alive) {
+                continue;
+            }
 
-            ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+
+    ctx.fillStyle = DEAD_COLOR;
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+            if (cells[idx] !== Cell.Dead) {
+                continue;
+            }
 
             ctx.fillRect(
                 col * (CELL_SIZE + 1) + 1,
